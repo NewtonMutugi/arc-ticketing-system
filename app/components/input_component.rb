236e99c13656eacb4a691f12a1bb909478797798
NewtonMutugi <newtonmut197@gmail.com>
2026-01-07 23:20:19 +0300

    feat: Update forgot password page and new password page to use consistent UI with login. Also update Input component to accept autocomplete args

--- app/components/input_component.rb
+++ app/components/input_component.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class InputComponent < ViewComponent::Base
-  def initialize(name:, label:, id: nil, type: "text", placeholder: " ", value: nil, required: true)
+  def initialize(name:, label:, id: nil, type: "text", placeholder: " ", value: nil, required: true, autocomplete: "")
     @name = name
     @label = label
     @id = id || name
     @type = type
     @placeholder = placeholder
     @value = value
-    @required = required
+    @required = required,
+    @autocomplete = autocomplete
   end
 end
