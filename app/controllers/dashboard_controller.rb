6d010dd56c272d3393a6f60b3939f37fff48ed5b
NewtonMutugi <newtonmut197@gmail.com>
2026-01-16 23:43:06 +0300

    feat: Add tiles to dasboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
   layout "dashboard"
   def index
     @user = Current.user
+    @events = Event.all
+    @tickets = Ticket.all
+    @revenue = Ticket.sum(:price)
   end
 end
