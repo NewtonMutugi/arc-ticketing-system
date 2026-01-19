eb32a86ac917164493e4d13e3c7ea01ff380333a
NewtonMutugi <newtonmut197@gmail.com>
2026-01-19 12:12:54 +0300

    feat: Add event image to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   has_many :tickets, dependent: :destroy
+
+  validates :title, :description,  presence: true
+
+  has_one_attached :event_image
+
+  validates :event_image, content_type: [ "image/png", "image/jpeg" ], size: { less_than: 5.megabytes }
 end
