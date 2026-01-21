7c083449b7c818522e4b0be69ff975522c32da92
NewtonMutugi <newtonmut197@gmail.com>
2026-01-21 22:18:38 +0300

    fix: Update link paths to use admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,8 @@ class Admin::DashboardController < Admin::BaseController
   layout "dashboard"
   def index
     @user = Current.user
-    @events = Event.all
-    @tickets = Ticket.all
-    @revenue = Ticket.sum(:price)
+    @events = Event.count
+    @tickets_sold = Event.all.sum(&:tickets_sold)
+    @revenue = Event.all.sum(&:revenue)
   end
 end
