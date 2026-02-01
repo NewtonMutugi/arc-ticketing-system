8d6e53a5d7f2ab4acfb7bb89d1a173796dd97343
NewtonMutugi <newtonmut197@gmail.com>
2026-02-01 22:40:42 +0300

    fix: Update session routes and adjust sign out links in dashboard layouts

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root "dashboard#index"
 
-    resource :session
+    # resource :session
+    get "login",  to: "sessions#new",     as: :new_session
+    post "login",  to: "sessions#create",  as: :session
+    delete "logout", to: "sessions#destroy", as: :destroy_session
     resources :passwords, param: :token
 
     resources :events do
