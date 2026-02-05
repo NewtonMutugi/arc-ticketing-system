1576ddf857f4e0c0c72be302e4465876311cd799
NewtonMutugi <newtonmut197@gmail.com>
2026-02-05 11:50:08 +0300

    feat: Add migration to backfill submitted orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_31_094549) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_081740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
