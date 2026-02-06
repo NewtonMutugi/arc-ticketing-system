9aefb8059573bb009daa1bf67a9b2319c314cca3
NewtonMutugi <newtonmut197@gmail.com>
2026-02-06 22:59:05 +0300

    fix: Update action mailer default URL host for ticketing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "ticket.rubyconf.africa", protocol: "https" }
+  config.action_mailer.default_url_options = { host: "tickets.rubyconf.africa", protocol: "https" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   config.action_mailer.smtp_settings = {
