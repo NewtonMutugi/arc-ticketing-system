0f5b8760dc3130c31611994fcd7d34a254c834d4
NewtonMutugi <newtonmut197@gmail.com>
2026-07-02 15:07:50 +0300

    feat: Add tests for editing user roles and updating via turbo stream

--- test/controllers/admin/settings_controller_test.rb
+++ test/controllers/admin/settings_controller_test.rb
@@ -25,4 +25,34 @@ class Admin::SettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "654321", Setting.mpesa_business_number
     assert_equal "60", Setting.session_timeout
   end
+
+  test "should get edit user role modal" do
+    target_user = User.where.not(id: @user.id).first
+    get edit_admin_user_path(target_user)
+    assert_response :success
+  end
+
+  test "should update a user role" do
+    target_user = User.where.not(id: @user.id).first
+
+    patch admin_user_path(target_user), params: {
+      role: "editor"
+    }
+
+    assert_redirected_to admin_settings_path
+    assert_equal "editor", target_user.reload.role
+  end
+
+  test "should update a user role via turbo stream" do
+    target_user = User.where.not(id: @user.id).first
+
+    patch admin_user_path(target_user), params: {
+      user: { role: "editor" }
+    }, as: :turbo_stream
+
+    assert_response :success
+    assert_equal "editor", target_user.reload.role
+    assert_match /turbo-stream action="replace" target="user_#{target_user.id}"/, response.body
+    assert_match /turbo-stream action="update" target="modal"/, response.body
+  end
 end
