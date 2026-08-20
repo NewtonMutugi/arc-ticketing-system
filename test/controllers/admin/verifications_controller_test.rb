340adfd35f519c73ee5409f1ef5521a68917ea65
NewtonMutugi <newtonmut197@gmail.com>
2026-08-20 07:12:08 +0300

    fix: update URL helper in verifications controller test and remove unused checked_in_at field from attendees fixture

--- test/controllers/admin/verifications_controller_test.rb
+++ test/controllers/admin/verifications_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class Admin::VerificationsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     sign_in_as(User.take)
-    get admin_verify_attendee_url(token: "test")
+    get verify_attendee_url(token: "test")
     assert_response :not_found
   end
 end
